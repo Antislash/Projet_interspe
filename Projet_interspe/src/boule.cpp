10e1b4e3c309740920acdd15f0d2b522d158f01c
Gataf <gtaffor@gmail.com>
2016-06-20 15:53:15 +0200

    recup modif max

--- Projet_interspe/src/boule.cpp
+++ Projet_interspe/src/boule.cpp
@@ -14,15 +14,26 @@ Boule::Boule(Point c, double r)
     center = c;
     radius = r;
     ballon=new MeshObj("models/untitled.obj");
-//    text = texture;
 }
 
 
 void Boule::render()
 {
 
-    glTranslatef(center.x,center.y,center.z);
-    glScaled(0.5,0.5,0.5);
-    ballon->draw_model();
+//    glTranslatef(center.x,center.y,center.z);
+//    glScaled(0.5,0.5,0.5);
+//    ballon->draw_model();
+
+    glEnable(GL_TEXTURE_2D);
+
+    GLUquadricObj *quadric = gluNewQuadric();
+    glColor3f(1,1,1);
+    gluQuadricTexture(quadric, GL_TRUE);
+
+    glBindTexture(GL_TEXTURE_2D, loadTexture("models/Ball_15.jpg" ));
+    gluSphere(quadric, radius, 32, 32);
+
+    gluDeleteQuadric(quadric);
+    glDisable(GL_TEXTURE_2D);
 
 }
